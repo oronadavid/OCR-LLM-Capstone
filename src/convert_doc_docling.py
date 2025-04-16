81a8af27aeeadd59f26ed4789a5bed787f164b4d
oronadavid <davidorona2@gmail.com>
2025-04-15 21:50:23 -0700

    Setup docling to use Tesseract, EasyOCR, RapidOCR, and OCR mac

--- src/convert_doc_docling.py
+++ src/convert_doc_docling.py
@@ -1,24 +1,28 @@
-from pathlib import Path
-from docling.datamodel.base_models import InputFormat
-from docling.datamodel.pipeline_options import PdfPipelineOptions, PipelineOptions, RapidOcrOptions
-from docling.document_converter import DocumentConverter, ImageFormatOption
 import os
+from pathlib import Path
 from huggingface_hub import snapshot_download
 
-source = Path("/home/david/Projects/school/capstone/ocr-llm-setup/images/test_images/test1.png")
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import EasyOcrOptions, OcrMacOptions, PdfPipeline, PdfPipelineOptions, PipelineOptions, RapidOcrOptions, TesseractOcrOptions
+from docling.document_converter import DocumentConverter, ImageFormatOption, PdfFormatOption
+from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend, PyPdfiumPageBackend
+from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
+from docling.pipeline.simple_pipeline import SimplePipeline
+
+source = Path("/home/david/Projects/school/capstone/ocr-llm-setup/images/test_images/test2.png")
 
 def load_rapid_ocr_model(det_model: str, rec_model: str, cls_model: str) -> DocumentConverter:
     print("Downloading RapidOCR models")
     download_path = snapshot_download(repo_id="SWHL/RapidOCR")
 
     det_model_path = os.path.join(
-        download_path, "PP-OCRv4", "ch_PP-OCRv4_det_server_infer.onnx"
+        download_path, det_model
     )
     rec_model_path = os.path.join(
-        download_path, "PP-OCRv4", "ch_PP-OCRv4_rec_server_infer.onnx"
+        download_path, rec_model
     )
     cls_model_path = os.path.join(
-        download_path, "PP-OCRv3", "ch_ppocr_mobile_v2.0_cls_train.onnx"
+        download_path, cls_model
     )
 
     ocr_options = RapidOcrOptions(
@@ -35,10 +39,83 @@ def load_rapid_ocr_model(det_model: str, rec_model: str, cls_model: str) -> Docu
         format_options={
             InputFormat.IMAGE: ImageFormatOption(
                 pipeline_options=pipeline_options
-            ),
+            )
         }
     )
 
+    return doc_converter
+
+def load_ocr_mac_model() -> DocumentConverter:
+    ocr_options = OcrMacOptions(
+        framework='vision'
+    )
+
+    pipeline_options = PdfPipelineOptions(
+        ocr_options=ocr_options
+    )
+
+    doc_converter = DocumentConverter(
+        allowed_formats=[
+            InputFormat.PDF,
+            InputFormat.IMAGE
+        ],
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend, pipeline_options=pipeline_options
+            ),
+            InputFormat.IMAGE: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend, pipeline_options=pipeline_options
+            )
+        }
+    )
+    
+    return doc_converter
+
+def load_tesseract_model() -> DocumentConverter:
+    ocr_options = TesseractOcrOptions()
+
+    pipeline_options = PdfPipelineOptions(
+        ocr_options=ocr_options
+    )
+
+    doc_converter = DocumentConverter(
+        allowed_formats=[
+            InputFormat.PDF,
+            InputFormat.IMAGE
+        ],
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend, pipeline_options=pipeline_options
+            ),
+            InputFormat.IMAGE: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend, pipeline_options=pipeline_options
+            )
+        }
+    )
+
+    return doc_converter
+
+def load_easy_ocr_model() -> DocumentConverter:
+    ocr_options = EasyOcrOptions()
+
+    pipeline_options = PdfPipelineOptions(
+        ocr_options=ocr_options
+    )
+
+    doc_converter = DocumentConverter(
+        allowed_formats=[
+            InputFormat.PDF,
+            InputFormat.IMAGE
+        ],
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend, pipeline_options=pipeline_options
+            ),
+            InputFormat.IMAGE: PdfFormatOption(
+                pipeline_cls=StandardPdfPipeline, backend=PyPdfiumDocumentBackend, pipeline_options=pipeline_options
+            )
+        }
+    )
     return doc_converter
 
 def image_to_text(document_converter: DocumentConverter):
@@ -47,5 +124,12 @@ def image_to_text(document_converter: DocumentConverter):
 
 
 if __name__ == "__main__":
-    document_converter = load_rapid_ocr_model("","","")
+    #document_converter = load_rapid_ocr_model(
+    #    "PP-OCRv4/ch_PP-OCRv4_det_server_infer.onnx",
+    #    "PP-OCRv3/ch_PP-OCRv3_rec_infer.onnx",
+    #    "PP-OCRv3/ch_ppocr_mobile_v2.0_cls_train.onnx"
+    #)
+    #document_converter = load_tesseract_model()
+    #document_converter = load_easy_ocr_model()
+    document_converter = load_ocr_mac_model()
     image_to_text(document_converter)
